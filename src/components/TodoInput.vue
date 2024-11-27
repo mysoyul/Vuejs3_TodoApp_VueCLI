b3b2e7e39296e04bdee39db5f79ca3cb1878802e
mysoyul <vega2k_@naver.com>
2024-11-27 15:51:01 +0900

    TodoInput clearTodo함수추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <input type="text" :value="newTodoItem" @input="handleInput">
+        <!-- <input type="text" v-model="newTodoItem"> -->
         <button @click="addTodo">추가</button>
     </div>
 </template>
@@ -10,20 +11,27 @@ import { ref } from 'vue'
 
 const newTodoItem = ref("")
 
+//사용자정의 Event를 선언함
 const emit = defineEmits(["input:todo"])
+
 const handleInput = (event) => {
     const todoText = event.target.value
     if (!todoText) return
+    //사용자정의 Event를 발생시킨다
     emit("input:todo", todoText)
     newTodoItem.value = todoText
 }
 
 const addTodo = () => {
     const todoItem = newTodoItem.value
+    //Input 필드에서 입력한 값을 로컬스토리지에 저장하기
     localStorage.setItem(todoItem, todoItem)
-    newTodoItem.value = ""
+    clearInput()
 }
 
+const clearInput = () => {
+    newTodoItem.value = ""
+}
 </script>
 
 <style scoped></style>
