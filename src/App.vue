aecd4910ddfa13e7a7836178b0b4478e32e2473c
mysoyul <vega2k_@naver.com>
2024-11-27 15:15:20 +0900

    App.vue에서하위컴포넌트호출

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from '@/components/TodoList.vue';
+import TodoFooter from '@/components/TodoFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoHeader, TodoInput, TodoList, TodoFooter
   }
 }
 </script>
