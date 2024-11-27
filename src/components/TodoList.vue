50fd044e2d2d9f0e32c458c74c7e18dfa6da1008
mysoyul <vega2k_@naver.com>
2024-11-27 16:26:04 +0900

    TodoList onBeforeMount 사용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,9 +5,20 @@
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+const todoItems = ref([])
 
+//Life cycle Hook
+onBeforeMount(() => {
+    console.log('mounted in the composition api!')
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageKey = localStorage.key(i)
+            todoItems.value.push(storageKey)
+        }
+    }
+    console.log(todoItems.value)
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
