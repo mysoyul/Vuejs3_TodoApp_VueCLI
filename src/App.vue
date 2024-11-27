191c4138fb2809bb4a665a372b2d8ce3ccc18001
mysoyul <vega2k_@naver.com>
2024-11-27 15:19:47 +0900

    App.vue style추가/TodoHeader제목추가

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
+import TodoHeader from '@/components/TodoHeader.vue';
+import TodoInput from '@/components/TodoInput.vue';
 import TodoList from '@/components/TodoList.vue';
 import TodoFooter from '@/components/TodoFooter.vue';
 
@@ -30,5 +30,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
