d45c2a931368ad613b289da76423d5d0f57f267a
mysoyul <vega2k_@naver.com>
2024-11-27 17:24:25 +0900

    TodoFooter clearTodo함수추가

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,19 +1,35 @@
 <template>
-    <div>
-        <div>TodoFooter</div>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
+    setup() {
+        const clearTodo = () => {
+            localStorage.clear()
+        }
 
-        return {}
+        return {
+            clearTodo
+        }
     }
 }
 </script>
 
 <style scoped>
+.clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+}
 
+.clearAllBtn {
+    color: #e20303;
+    display: block;
+}
 </style>
