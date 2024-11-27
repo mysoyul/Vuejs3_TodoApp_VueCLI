d9f030b9f193b0aaf55601e79693b44591079a86
mysoyul <vega2k_@naver.com>
2024-11-27 15:27:27 +0900

    TodoInput ref()추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <div>TodoInput</div>
+        <input type="text" :value="newTodoItem">
+        <button>추가</button>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const newTodoItem = ref("")
 
 </script>
 
