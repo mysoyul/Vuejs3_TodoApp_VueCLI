203823887bc0e9d8bdb82db4d0258523785beb47
mysoyul <vega2k_@naver.com>
2024-11-28 14:15:06 +0900

    store todoItems상태변수초기화

--- src/store/index.js
+++ src/store/index.js
@@ -3,26 +3,10 @@ import axios from "axios"
 
 import http from "@/common/http-common"
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                const storageKey = localStorage.key(i)
-                if (storageKey !== '__VUE_DEVTOOLS_KIT_TIMELINE_LAYERS_STATE__') {
-                    const itemJson = localStorage.getItem(storageKey)
-                    arr.push(JSON.parse(itemJson))
-                }
-            }
-        }
-        return arr;
-    }
-}
-
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
     state: {
-        todoItems: storage.fetch()
+        todoItems: []
     },
     actions: {
         loadTodoItems({ commit }) {
