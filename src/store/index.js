26a8fee86bfbc1a9b5cb723e65dd74d3bcd28988
mysoyul <vega2k_@naver.com>
2024-11-28 14:38:49 +0900

    store index.js에서 modules사용

--- src/store/index.js
+++ src/store/index.js
@@ -1,87 +1,9 @@
 import { createStore, createLogger } from "vuex"
-import axios from "axios"
-
-import http from "@/common/http-common"
+import { moduleTodo } from "./modules/moduleTodo"
 
 export const store = createStore({
-    plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
-    state: {
-        todoItems: []
-    },
-    actions: {
-        loadTodoItems({ commit }) {
-            http
-                .get('/todos')
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-                .catch(error => {
-                    if (axios.isAxiosError(error)) {
-                        console.log(`StatusCode = ${error?.response?.status} : ErrorMessage = ${error.message}`)
-                    } else {
-                        console.error(error);
-                    }
-                });
-        }, //loadTodoItems
-        removeTodo({ commit }, payload) {
-            http
-                .delete(`/todos/${payload.id}`)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }, //removeTodo
-        addTodo({ commit }, payload) {
-            http
-                .post(`/todos`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }, //addTodo
-        toggleTodo({ commit }, payload) {
-            http
-                .patch(`/todos/${payload.id}`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },//toggleTodo
-        clearTodo({ commit }) {
-            http
-                .delete('/todos')
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },//clearTodo
-
-    },
-    mutations: {
-        setTodoItems(state, items) {
-            state.todoItems = items;
-        },
-        addTodo(state, todoItem) {
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeTodo(state, payload) {
-            const { todo: { item }, index } = payload
-            localStorage.removeItem(item);
-            state.todoItems.splice(index, 1);
-        },
-        toggleTodo(state, payload) {
-            const { todo: { item, completed }, index } = payload
-            state.todoItems[index].completed = !completed
-            localStorage.removeItem(item);
-            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
-        },
-        clearTodo(state) {
-            localStorage.clear()
-            state.todoItems = []
-        }
-    }, //mutations
-
+    plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],    
+    modules: {
+        moduleTodo
+    }
 })
